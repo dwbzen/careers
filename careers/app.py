db79c3fa9d9e09a7cff14e1d3ca82c24661b2051
Brian Bacon <brian.bacon01@gmail.com>
2022-09-16 22:30:38 -0400

    Added route to create a new game.

--- careers/app.py
+++ careers/app.py
@@ -1,13 +1,21 @@
 import uvicorn
 from fastapi import FastAPI, Depends
-from server.gameManager import CareersGameManager
 
+from careers.server.gameManager import CareersGameManager
+from careers.game.careersGameEngine import CareersGameEngine
+
+gameEngine = CareersGameEngine()
 manager = CareersGameManager()    
 app = FastAPI()
 
 @app.get("/", status_code=200)
 def get(game: CareersGameManager=Depends(manager)):
     return {}
+
+@app.post('/game/{points}', status_code=201)
+def createGame(points: int, installationId: str, gameInstance: CareersGameEngine=Depends(manager)):
+    """Creates a new game and returns the game id"""
+    return gameInstance.create("Hi-Tech", 'points', installationId, points)
 
 if __name__ == "__main__":
     uvicorn.run("app:app", port=9000, reload=True)
--- careers/server/gameManager.py
+++ careers/server/gameManager.py
@@ -4,10 +4,22 @@
 
 from typing import Any
 
+from careers.game.careersGameEngine import CareersGameEngine
+
 class CareersGameManager(object):
 
     def __init__(self):
+        self.games = {}
         pass
 
-    def __call__(self) -> Any:
-        return self
+    def create(self):
+        pass
+
+    def __call__(self, installationId: str) -> Any:
+        """Create a new game engine for the user and return the instance"""
+        game = self.games.get(installationId)
+    
+        if game is None:
+            self.games[installationId] = list()
+            self.games[installationId].append(CareersGameEngine())
+        return self.games[installationId][len(self.games[installationId]) -1]
