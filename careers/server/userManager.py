84d47ff2317d588c829f476a7848ee92230796d9
Brian Bacon <brian.bacon01@gmail.com>
2023-01-15 21:54:08 -0500

    removed old code

--- careers/server/userManager.py
+++ careers/server/userManager.py
@@ -6,11 +6,10 @@ from uuid import uuid4
 
 import dotenv
 from pydantic import BaseModel, Field
-from pydantic.utils import GetterDict
 from pymongo import MongoClient
 
 
-class User(BaseModel, object):
+class User(BaseModel,):
     name: str = Field(...)
     email: str = Field(...)
     initials: str = Field(...)
